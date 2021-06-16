a5de7cf1a562f05b5714a9ee9ada76e7bf254a07
Icaro Oliveira <icarovinici@gmail.com>
2021-06-16 18:05:38 -0300

    feat: add initial redux structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
 
 import { theme } from 'styles/theme'
 import { Layout } from 'components'
+import { store } from 'redux/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </Provider>
   )
 }
 
